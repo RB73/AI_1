a009f36b8b00ae5da6c6db1216472d860fb61063
Rayan <raalsoby@wpi.edu>
2016-09-15 16:39:38 -0400

    Paul is in

--- src/AIPack1/Organism.java
+++ src/AIPack1/Organism.java
@@ -5,34 +5,40 @@ import java.util.ArrayList;
 public class Organism {
 	private float start;
 	private float goal;
-	private ArrayList<Function> operations;
+	private ArrayList<Integer> operations;
 	private float fitnessFunction;
 	private float error;
 	private int size; //number of operations the organism has
-
-	public Organism (float start, float goal, ArrayList<Function> operations){
+	private AIMath math;
+	
+	public Organism (float start, float goal, ArrayList<Integer> operations, AIMath math){
 		this.start = start;
 		this.goal = goal;
 		this.operations = operations;
 		this.size = operations.size();
+		this.math = math;
 	}
 	
 	
 	
 	//TODO write the calculations of the error from the the operations and start
-	public float calcError(){
-		return 0;
+	public void calcError(){
+		float result = this.start;
+		for (int i = 0; i < this.size; i++){
+			result = this.math.Op(this.operations.get(i), result);
+		}
+		this.error = Math.abs(this.goal - result);
 	}
 	
 	//TODO write up the calculation for this function using the error and size 
-	public float calcFitnessFunction() {
-		return 0;
+	public void calcFitnessFunction() {
+		this.fitnessFunction = this.error + this.size;
 	}
 	
 	
 	// getters
 	
-	public ArrayList<Function> getOperations() {
+	public ArrayList<Integer> getOperations() {
 		return operations;
 	}
 
@@ -49,11 +55,11 @@ public class Organism {
 	}
 
 	public Organism mutate(){ // mutate this organism
-		return new Organism(0,0,null);
+		return new Organism(0,0,null, math);
 	}
 	
 	public Organism parent(Organism parent){ // create new organism based on this and one other
-		return new Organism(0,0,null);
+		return new Organism(0,0,null, math);
 	}
 	
 	
