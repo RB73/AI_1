fbb228afaf6965b433ab64fdb893735e4248e818
Larisa Motova <larisa@motovs.org>
2016-09-15 16:12:36 -0400

    Changed arrays into ArrayList in PopulationReducer

--- src/AIPack1/PopulationReducer.java
+++ src/AIPack1/PopulationReducer.java
@@ -1,5 +1,9 @@
 package AIPack1;
 
+import com.sun.org.apache.xpath.internal.operations.Or;
+
+import java.util.ArrayList;
+
 /**
  *
  */
@@ -10,93 +14,72 @@ public class PopulationReducer {
 
 
 //    private float median;        // this is the middle of the fitness function result // not used whoops
-    private Organism[] organisms;
-    private boolean[] toKeep; // used in reference of the original organisms array
+    private ArrayList<Organism> organisms;
     private int length;
 
 
 
-    PopulationReducer(Organism[] organisms){
+    PopulationReducer(ArrayList<Organism> organisms){
         this.organisms = organisms;
-        length = this.organisms.length;
+        length = this.organisms.size();
         quickSort(this.organisms, 0, length - 1);
 //        median = this.organisms[length / 2 - 1].getFitnessFunction();
-        toKeep = new boolean[length];
     }
 
     // NOTE: there are two reduce functions (both should work, choose whichever)
 
 
     // linear probability function (if there's 100 organisms, 100% for best surviving, 99% for second best, etc..)
-    private Organism[] reduce(){
+    private ArrayList<Organism> reduce(){
         int probability;    // percentage talked about ^
         int keepLength = 0; // count for length of how many organisms survive
+
+        ArrayList<Organism> finalList = new ArrayList<Organism>();
 
         // figures out whether to keep an organism
         for(int i = 0; i < length; i ++){
             probability = 100 * (length - i) / length;
-            if(100 * Math.random() < probability) {
-                toKeep[i] = true;
-                keepLength++;
-            }
-            else toKeep[i] = false;
+            if(100 * Math.random() < probability)
+                finalList.add(organisms.get(i));
         }
 
-        // moves the list of organisms to keep over to a final array
-        return makeFinalList(keepLength);
+        return finalList;
     }
 
 
     // split organisms into 2 sections, give probability% chance of survival for lower half, probability% death for upper half
-    private Organism[] reduce2(){
+    private ArrayList<Organism> reduce2(){
         int keepProbability = (int)(100 * PERCENT_FLIPPED);
         int keepLength = 0;
 
+        ArrayList<Organism> finalList = new ArrayList<Organism>();
 
         for(int i = 0; i < length / 2; i++){        // to keep (from worse half)
-            if(100 * Math.random() < keepProbability){
-                toKeep[i] = true;
-                keepLength++;
-            } else toKeep[i] = false;
+            if(100 * Math.random() < keepProbability)
+                finalList.add(organisms.get(i));
         }
         for(int i = length / 2; i < length; i++){   // to keep (from better half)
-            if(100 * Math.random() < 100 - keepProbability){
-                toKeep[i] = true;
-                keepLength++;
-            } else toKeep[i] = false;
+            if(100 * Math.random() < 100 - keepProbability)
+                finalList.add(organisms.get(i));
         }
 
-        // moves the list of organisms to keep over to a final array
-        return makeFinalList(keepLength);
-    }
-
-
-    private Organism[] makeFinalList(int finalLength){
-        Organism[] keptOrganisms = new Organism[finalLength];
-        int count = 0;
-        for(int i = 0; i < toKeep.length; i++){
-            if(toKeep[i]){
-                keptOrganisms[count] = organisms[i];
-                count++;
-            }
-        }
-        return keptOrganisms;
+        return finalList;
     }
 
 
     // https://en.wikipedia.org/wiki/Quicksort
-    private void quickSort(Organism[] toSort, int high, int low){
+    private void quickSort(ArrayList<Organism> toSort, int high, int low){
         if(low < high){
             int pivot = partition(toSort, low, high);
             quickSort(toSort, low, pivot - 1);
             quickSort(toSort, pivot + 1, high);
         }
     }
-    private int partition(Organism[] toSort, int high, int low){
-        float pivot = toSort[high].getFitnessFunction();
+    private int partition(ArrayList<Organism> toSort, int high, int low){
+        float pivot = toSort.get(high).getFitnessFunction();
         int i = low;
         for(int j = low; j < high - 1; j++){
-            if(toSort[j].getFitnessFunction() <= pivot){
+            if(toSort.get(j).getFitnessFunction() <= pivot){
                 swap(toSort, i, j);
                 i++;
             }
@@ -104,10 +87,10 @@ public class PopulationReducer {
         swap(toSort, i, high);
         return i;
     }
-    private void swap(Organism[] toSort, int first, int second){
-        Organism temp = toSort[first];
-        toSort[first] = toSort[second];
-        toSort[second] = temp;
+    private void swap(ArrayList<Organism> toSort, int first, int second){
+        Organism temp = toSort.get(first);
+        toSort.set(first, toSort.get(second));  // does this work or is it just pointers
+        toSort.set(second, temp);
     }
 
 
